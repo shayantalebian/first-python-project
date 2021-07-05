18ec8f5c0ffb2a728dd9458515c4709e3b1a41e0
Shayan Talebian <shayantalebianwork@gmail.com>
2021-07-06 01:00:48 +0430

    changing color

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ g = turtle.Screen()
 g.bgcolor("black")
 t.width(2)
 t.speed(15)
-col=("yellow" , "green" , "orange" , "blue")
+col=("yellow" , "green" , "red" , "blue")
 for i in range (300) :
     t.pencolor(col[i%4])
     t.forward(i*4)
